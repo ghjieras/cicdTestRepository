400c231e4f73e4e7a2a38015a91794f276e7197c
wade <ghjieras@gmail.com>
2020-10-26 16:47:19 +0800

    master test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var assert = require('assert')
 
 describe('Array', () => {
- describe('indexOf()', () => {
+  describe('indexOf()', () => {
    it('test', () => {
      assert.equal(-1, [1,2,3].indexOf(0))
      console.log('CICD HELLO WORLD')
